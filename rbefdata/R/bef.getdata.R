1d0d3e203de788950871966cc4ea78ff5f62c0fb
cpfaff <ctpfaff@arcor.de>
2013-04-10 09:42:03 +0200

    Refactoring of the getdata wrapper function

--- rbefdata/R/bef.getdata.R
+++ rbefdata/R/bef.getdata.R
@@ -1,22 +1,22 @@
-#' This is a wrapper to the getter functions of the befdata R package. 
+#' This is a wrapper for the getter functions of the befdata R package. 
 #'
 #' It returns contens depending on the on the parameters you provide to the function. 
-#' It will fetch a dataset if you provide the id or even all dataset associated with 
-#' a paper proposal based on the proposal id. You can even provide the function with 
-#' a full url and it will fetch the right contents. 
+#' It will fetch a dataset if you provide the id. If you provide a proposal id it will 
+#' fetch all datasets associated with that proposal. You can even provide the function with 
+#' a full url and it will fetch the right contents by parsing the url. 
 #'
 #' @param dataset_id This is the id of a dataset on a BEFdata portal. You find the id 
 #'        in the url of the dataset or on the download page of the dataset. 
 #' @param user_credentials This are the login credentials required to verify 
-#'        the users identity. They ensure you have the proper rights to download the data.
-#' @param full_url This option workds as direct download link. It can be used instead of 
+#'        the user identity. They ensure, you have the proper rights to download the data.
+#' @param full_url This option works as direct download link. It can be used instead of 
 #'        the according ids. You can find the url either on the dataset download page for 
-#'        a dataset or on the proposal page for a proposal. Simply copy and paste it from 
-#'        the browser.
+#'        a dataset or on the proposal page for a paper proposal.
 #' 
 #' @return It returns a dataframe in case of a single dataset and a list of objects in case 
 #'         of a paper proposal. An error is thrown when the dataset or proposal is not found 
-#'         or you don't have the proper access right to perform the action.
+#'         or you don't have the proper access right to perform the action. For detailed 
+#'         informations about the the used functions see bef.getData() and bef.getProposal().
 #' 
 #' @examples \dontrun{
 #'  dataset = bef.getdata(dataset_id=8, user_credentials="Yy2APsD87JiDbF9YBnU")
@@ -28,27 +28,29 @@
 
 bef.getdata <- function(dataset_id, proposal_id, full_url, user_credentials) {
   if (!missing(full_url)) {
-    # check for content to fetch
-    split_full_url = unlist(strsplit(full_url, split="/"))
-      if (!is.null(grep("datasets", split_full_url))) {
+    # check which content to fetch
+    if(grepl(full_url, pattern = "datasets")) 
+      {     
         dataset=bef.getDataset(full_url=full_url)  
         return(dataset)
-      }
-      if (!is.null(grep("paperproposals", split_full_url))) {
+      } 
+    if(grepl(full_url, pattern="paperproposals"))
+      {
         proposal=bef.getProposal(full_url=full_url)   
         return(proposal)
       }  
   }
   
   # fetch via dataset_id
-  if (!missing(dataset_id)) {  
-    dataset=bef.getDataset(dataset_id=dataset_id, user_credentials=user_credentials)
-    return(dataset)
-  }
- 
+  if (!missing(dataset_id)) 
+    {  
+      dataset=bef.getDataset(dataset_id=dataset_id, user_credentials=user_credentials)
+      return(dataset)
+    }
   # fetch via proposal_id
-  if (!missing(proposal_id)) {  
-    proposal=bef.getProposal(proposal_id=proposal_id, user_credentials=user_credentials)
-    return(proposal)
-  } 
+  if (!missing(proposal_id)) 
+    {  
+      proposal=bef.getProposal(proposal_id=proposal_id, user_credentials=user_credentials)
+      return(proposal)
+    } 
 }
