e010f70554bb5257402671602377b60b0abde32f
cpfaff <ctpfaff@arcor.de>
2013-04-11 11:44:15 +0200

    Removes files not required and moves its contents into the new helper file.

--- rbefdata/R/bef.helper.R
+++ rbefdata/R/bef.helper.R
@@ -1,53 +1,37 @@
-#' This is a helper file that contains functions used by the BEFdata R package 
+#' This is a helper file that contains functions used by the BEFdata R package
 #' Internally.
 #'
-#' @import RCurl 
+#' @import RCurl
 #' @export
 
-bef.combineObjects <- function(dataset_object, dataset_url, metadata_object, metadata_url) 
-  {  
-    if(!missing(metadata_url)) 
-      { 
-        metadata = bef.getMetadata(full_url=metadata_url)  
+bef.combineObjects <- function(dataset_object, dataset_url, metadata_object, metadata_url)
+  {
+    if(!missing(metadata_url))
+      {
+        metadata = bef.getMetadata(full_url=metadata_url)
         dataset = dataset_object
-      } 
-    
-    if(!missing(dataset_url)) 
-      { 
-        dataset = bef.getDataset(full_url=dataset_url) 
-        metadata = metadata_objet 
-      } 
+      }
+
+    if(!missing(dataset_url))
+      {
+        dataset = bef.getDataset(full_url=dataset_url)
+        metadata = metadata_objet
+      }
 
     if(!missing(dataset_object) && !missing(metadata_object))
-      { 
-        dataset = dataset_object 
+      {
+        dataset = dataset_object
         metadata = metadata_object
-      } 
+      }
 
     attr(dataset, "title") = metadata$title
     attr(dataset, "authors") = metadata$authors
     attr(dataset, "abstract") = metadata$abstract
     attr(dataset, "taxonicextent") = metadata$taxonicextent
     attr(dataset, "spatial_information") = metadata$spatial
-    attr(dataset, "temporal_information") = metadata$temporal 
-    attr(dataset, "sampling") = metadata$temporal 
+    attr(dataset, "temporal_information") = metadata$temporal
+    attr(dataset, "sampling") = metadata$temporal
     attr(dataset, "analyze") = metadata$analyze
 
     return(dataset)
   }
-
-bef.prepareUrl <- function (full_url, category_split=T) 
-  { 
-    # url options 
-    include_via = "?" 
-    append_via = "&" 
-
-    # no options set in url
-    if(!grepl(full_url, pattern=include_via))
-      {
-       a=2 
-      }
-
-    if(category_split) 
-      grep
-    }
