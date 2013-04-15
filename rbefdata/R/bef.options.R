1922c86b4734236177d3d4cfcd7b0aeb349941f1
cpfaff <ctpfaff@arcor.de>
2013-04-15 10:21:07 +0200

    Introduces bef.options(tematres_url="www.bar.de")
    The URL of the tematres server is now settable via the bef.options()
    This is discussed in and closes #11. The following examples show how
    it works:
    
    bef.options(url="befdatadevelepment.biow.uni-leipzig.de")
    bef.options("url")
    bef.options(url="www.fu.de", tematres_url="www.bar.de")
    bef.options("tematres_url")
    bef.options(tematres_url="www.baz.de")
    bef.options()

--- rbefdata/R/bef.options.R
+++ rbefdata/R/bef.options.R
@@ -1,39 +1,52 @@
-#' Set or query BEFdata portal URL address
-#' 
-#' This function is used to query and set the BEFdata portal URL address. 
-#' 
+#' Set or query BEFdata portal URL address and the Tematres thesaurus URL.
+#'
+#' This function is used to query and set R BEFdata package specific options.
+#'
 #' @param \dots similar to \code{\link{options}}. see examples below.
-#' @examples 
-#' # query BEFdata portal's URL
+#' @examples
+#' # query BEFdata portal URL
 #' bef.options('url')
-#' # set URL
+#' # set the URL
 #' bef.options(url='http://www.example.com')
+#' #Tematres url
+#' bef.options('temtres_url')
+#' bef.options(tematres_url="http://www.example.com")
+#'
 #' @export
 
 bef.options = function(...) {
-		lst = list(...)
-		.bef.opts = .bef.env$.bef.opts
-		if (length(lst)) {
-			if (is.null(names(lst)) && !is.list(lst[[1]])) {
-					lst = unlist(lst)
-					if (length(lst) == 1) .bef.opts[[lst]] else .bef.opts[lst]
-			}
-			else {
-				omf = .bef.opts
-				if (is.list(lst[[1]]))
-						lst = lst[[1]]
-				if (length(lst) > 0) {
-					.bef.opts[names(lst)] = lst
-					.bef.opts["url"] = sub(.bef.opts["url"], pattern = "(/)?$", replacement = "")
-					.bef.opts["url"] = sub(.bef.opts["url"], pattern = "^(http://)?", replacement = "http://")
-					.bef.env$.bef.opts = .bef.opts
-				}
-				invisible(omf)
-			}
-		} 
-		else {
-			.bef.opts
-		}
+  lst = list(...)
+  .bef.opts = .bef.env$.bef.opts
+  if (length(lst)) {
+    if (is.null(names(lst)) && !is.list(lst[[1]])) {
+      lst = unlist(lst)
+      if (length(lst) == 1) .bef.opts[[lst]] else .bef.opts[lst]
+    }
+    else {
+      omf = .bef.opts
+      if (is.list(lst[[1]]))
+        lst = lst[[1]]
+      if (length(lst) > 0) {
+
+        .bef.opts[names(lst)] = lst
+
+        if (!is.null(lst$url)) {
+          .bef.opts["url"] = sub(.bef.opts["url"], pattern = "(/)?$", replacement = "")
+          .bef.opts["url"] = sub(.bef.opts["url"], pattern = "^(http://)?", replacement = "http://")
+        }
+
+        if (!is.null(lst$tematres_url)) {
+          .bef.opts["tematres_url"] = .bef.opts["tematres_url"]
+        }
+
+        .bef.env$.bef.opts = .bef.opts
+      }
+      invisible(omf)
+    }
+  }
+  else {
+    .bef.opts
+  }
 }
 
 .bef.env = new.env()
