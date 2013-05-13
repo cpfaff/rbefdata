708a9edb325d6074b848cb28ecd3bb49f2d77094
manxingxing <manxingxing@gmail.com>
2013-05-13 12:07:34 +0800

    refactor bef.searchTematres

--- rbefdata/R/bef.searchTematres.R
+++ rbefdata/R/bef.searchTematres.R
@@ -7,29 +7,18 @@
 #' @import XML
 #' @export
 
-bef.searchTematres <- function(lookup_keyword)
-  {
-    service_task="search"
-    service_argument=lookup_keyword
-    service_url = sprintf("%s?task=%s&arg=%s", bef.options("tematres_service_url"), service_task, service_argument)
-
-    search_fetch_xml=getURL(service_url)
-    search_parse_xml=xmlTreeParse(search_fetch_xml, getDTD=F)
-    search_get_xml_root = xmlRoot(search_parse_xml)
-
-    if (length(grep("^result$",names(search_get_xml_root))) == 1)
-      {
-        search_get_results=search_get_xml_root[[1]]
-        search_get_results_names=""
-
-        for (i in 1:xmlSize(search_get_results))
-          {
-            search_get_results_names[i]=xmlSApply(search_get_xml_root[[1]][[i]][[2]], xmlValue)
-          }
-
-        return(search_get_results_names)
-
-      }else{
-        stop("The server says: No terms available for your search")
-      }
+bef.searchTematres <- function(lookup_keyword) {
+  service_task = "search"
+  service_argument = lookup_keyword
+  service_url = sprintf("%s?task=%s&arg=%s", bef.options("tematres_service_url"), service_task, service_argument)
+  search_fetch_xml = xmlParse(service_url)
+  items = xpathSApply(search_fetch_xml, path = "//result/term/string", xmlValue, ignoreComments=T)
+  if (length(items)) {
+    ids = xpathSApply(search_fetch_xml, path = "//result/term/term_id", xmlValue, ignoreComments=T)
+    attr(items, "id") = as.numeric(ids)
   }
+  else {
+    stop("The server says: No terms available for your search")
+  }
+  return(items)
+}
