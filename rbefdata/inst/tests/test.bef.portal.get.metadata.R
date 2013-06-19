817731b8fcc1203899924c83a6e6b478f278297e
cpfaff <ctpfaff@arcor.de>
2013-06-19 10:39:06 +0200

    Fixes the broken tests

--- rbefdata/inst/tests/test.bef.portal.get.metadata.R
+++ rbefdata/inst/tests/test.bef.portal.get.metadata.R
@@ -1,25 +1,25 @@
+source("test.helpers.R")
+
 context("Get metadata with bef.portal.get.metadata()")
 
 test_that("it downloads metadata for john doe", {
-  bef.options(url="http://befdatadevelepment.biow.uni-leipzig.de")
-  bef.options(user_credentials="")
+  given_the_user_is("invalid")
+  given_the_portal_is("development")
   expect_that(bef.portal.get.metadata(dataset_id=7), is_a("list"))
 })
 
 test_that("it throws an error if eml is not found", {
-  bef.options(url="http://befdatadevelepment.biow.uni-leipzig.de")
+  given_the_portal_is("development")
   expect_error(bef.portal.get.metadata(dataset_id=1000), "failed to load HTTP resource")
 })
 
 test_that("it gets metadata by id", {
-  bef.options(url="http://befdatadevelepment.biow.uni-leipzig.de")
-  bef.options(user_credentials="invalid")
+  given_the_portal_is("development")
   expect_that(bef.portal.get.metadata(dataset_id=7), is_a("list"))
 })
 
 test_that("it gets metadata by a full url", {
-  bef.options(url="http://befdatadevelepment.biow.uni-leipzig.de")
-  bef.options(user_credentials="invalid")
+  given_the_portal_is("development")
   expect_that(bef.portal.get.metadata(dataset_id=7), is_a("list"))
 })
 
