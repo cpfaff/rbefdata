98970b6325e202d96de7ae5e7d01b7ff5bd2ea20
cpfaff <ctpfaff@arcor.de>
2013-07-30 12:07:09 +0200

    Fixes typos

--- rbefdata/R/bef.portal.upload.dataset.R
+++ rbefdata/R/bef.portal.upload.dataset.R
@@ -2,10 +2,9 @@
 #'
 #' This function uploads a dataset from within R to a BEFdata portal set in
 #' your options. Before you can do so you need to have an account on the portal
-#' and set your credentials via bef.option() so the authentication works. The
-#' function requires a title for the dataset and either a path to a csv file for
-#' for upload or a dataset object. The dataset object needs to be a data frame
-#' with unique column names.
+#' and set your credentials via the bef.options() function so the authentication works.
+#' The function requires a title for the dataset and either a path to a CSV file for
+#' for upload or a dataset object. The dataset must have unique column names.
 #'
 #' @param dataset_title The title of the dataset.
 #' @param dataset_object The data frame you like to upload as dataset.
@@ -15,12 +14,11 @@
 #' @return Returns a status message with the ID of the dataset (not yet).
 #' @import RCurl
 #' @export
-#'
 
 bef.portal.upload.dataset <- bef.upload.dataset <- function(dataset_title, dataset_object, file_path, curl = getCurlHandle()) {
   if (missing(file_path)) {
-    write.csv(dataset_object, paste0(tempdir(), "/", "uplad_preparation.csv"), row.names=FALSE)
-    upload_file = fileUpload(paste0(tempdir(), "/", "uplad_preparation.csv"))
+    write.csv(dataset_object, paste0(tempdir(), "/", "upload_preparation.csv"), row.names=FALSE)
+    upload_file = fileUpload(paste0(tempdir(), "/", "upload_preparation.csv"))
   } else {
     upload_file = fileUpload(file_path)
   }
