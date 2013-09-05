923eb9c97c54751ac367a866c33ce668c2968578
cpfaff <ctpfaff@arcor.de>
2013-09-05 15:30:07 +0200

    Fixes not available function names

--- rbefdata/R/bef.portal.get.proposal.R
+++ rbefdata/R/bef.portal.get.proposal.R
@@ -27,7 +27,8 @@
 #'	  prop1 = bef.portal.get.datasets_for_proposal(proposal = 8)
 #'  	}
 #' @import RCurl
-#' @export
+#' @export bef.portal.get.datasets_for_proposal bef.get.datasets_for_proposal
+#' @alias bef.get.datasets_for_proposal
 
 bef.portal.get.datasets_for_proposal <- bef.get.datasets_for_proposal <- function(id, curl = getCurlHandle(), ...) {
   paperproposal_url = paperproposal_url(proposal_id = id)
