7a857b266f35ab91b523ba45f6ed70d70163fd31
cpfaff <ctpfaff@arcor.de>
2019-12-17 19:11:53 +0100

    Documents new deprecated functions

--- rbefdata/R/rbefdata-deprecated.R
+++ rbefdata/R/rbefdata-deprecated.R
@@ -7,14 +7,17 @@
 #'  \code{bef.portal.get.dataset_list} \tab \code{\link{bef.portal.get.datasets.for_keyword}} \cr
 #'  \code{bef.portal.get.proposal} \tab \code{\link{bef.portal.get.datasets.for_proposal}} \cr
 #'  \code{bef.tematres.api} \tab \code{\link{bef.tematres}} \cr
+#'  \code{bef.get.dataset} \tab \code{\link{get_dataset}} \cr
+#'  \code{bef.get.dataset_by} \tab \code{\link{get_dataset}} \cr
+#'  \code{bef.portal.get.dataset_by} \tab \code{\link{get_dataset}} \cr
+#'  \code{bef.portal.get.dataset} \tab \code{\link{get_dataset}} \cr
 #' }
-#'
+#' bef.portal.get.dataset_list
 #' @name rbefdata-deprecated
-#' @aliases bef.portal.get.dataset_list bef.portal.get.proposal bef.tematres.api
-#' @export bef.portal.get.dataset_list bef.portal.get.proposal bef.tematres.api
+#' @aliases bef.portal.get.dataset_list bef.portal.get.proposal bef.tematres.api bef.get.dataset bef.get.dataset_by bef.portal.get.dataset_by bef.portal.get.dataset
+#' @export bef.portal.get.dataset_list bef.portal.get.proposal bef.tematres.api bef.get.dataset bef.get.dataset_by bef.portal.get.dataset_by bef.portal.get.dataset
 #' @keywords internal
 
-## version 0.3 -> 0.4 deprecates
 #' @rdname rbefdata-deprecated
 bef.portal.get.dataset_list <- function(...) {
   .Deprecated("bef.portal.get.datasets_for_keyword")
@@ -30,5 +33,29 @@ bef.portal.get.proposal <- function(...) {
 #' @rdname rbefdata-deprecated
 bef.tematres.api <- function(...) {
   .Deprecated("bef.tematres")
-  bef.tematres.api(...)
+  bef.tematres(...)
+}
+
+#' @rdname rbefdata-deprecated
+bef.get.dataset <- function(...) {
+  .Deprecated("get_dataset")
+  get_dataset(...)
+}
+
+#' @rdname rbefdata-deprecated
+bef.get.dataset_by <- function(...) {
+  .Deprecated("get_dataset")
+  get_dataset(...)
+}
+
+#' @rdname rbefdata-deprecated
+bef.portal.get.dataset_by <- function(...) {
+  .Deprecated("get_dataset")
+  get_dataset(...)
+}
+
+#' @rdname rbefdata-deprecated
+bef.portal.get.dataset <- function(...) {
+  .Deprecated("get_dataset")
+  get_dataset(...)
 }
